51e96c98e65308a115de0a3185479a023a4c142b
Nick Hill <nickhill2380@gmail.com>
2020-10-27 22:00:23 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,14 +27,16 @@ var fight = function(enemyName) {
         if (confirmSkip){
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
         }
     };
     
 //Subract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
-enemyHealth=enemyHealth-playerAttack;
+var damage = randomNumber(playerAttack -3, playerAttack);
+
+enemyHealth = Math.max(0, enemyHealth - damage);
 //Log a resulting message to the console so we know that it worked
 console.log(
     playerName + " attacked " + enemyName +  ". " + enemyName + " now has " + enemyHealth + " health remaining."
@@ -54,7 +56,9 @@ if (enemyHealth <= 0){
     window.alert(enemyName + " still has " + enemyHealth + " health left.");
 }
 //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
-playerHealth=playerHealth-enemyAttack;
+var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+playerHealth = Math.max(0, playerHealth - damage);
 //Log a resulting message to the console so we know that it worked
 console.log(
     enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
@@ -89,14 +93,14 @@ for(var i = 0; i < enemyNames.length; i++) {
     var pickedEnemyName = enemyNames[i];
     
     //rest enemyHealth before starting new fight
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40,60);
 
     
 
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
 
-    if (playerHealth > 0 && i< enemyNames.length -1) {
+    if (playerHealth > 0 && i < enemyNames.length -1) {
         //ask if player wants to use the store before next round
         var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
         
@@ -185,6 +189,12 @@ var shop = function () {
             shop();
             break;
     }
+};
+
+var randomNumber = function (min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
 };
 //start the game when the page loads
 startGame();
