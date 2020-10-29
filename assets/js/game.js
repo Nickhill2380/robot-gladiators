9c2c875b67e7d11fbe26f1321d2308a015dc6b3c
Nick Hill <nickhill2380@gmail.com>
2020-10-29 09:03:42 -0400

    Improved shop input

--- assets/js/game.js
+++ assets/js/game.js
@@ -111,10 +111,6 @@ for(var i = 0; i < enemyInfo.length; i++) {
         }
     }
 }
-    else {
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
-    }
 };
 
 startGame();
@@ -144,22 +140,22 @@ else {
 var shop = function () {
     //ask player what they'd like to do
     var shopOptionPrompt = window.prompt (
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', OR 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
 
+    shopOptionPrompt = parseInt(shopOptionPrompt);
+        
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        
+        case 1:
             playerInfo.refillHealth();
             break;
-        
-        case "UPGRADE" :
-        case "upgrade" :
+                
+        case 2:
             playerInfo.upgradeAttack();
             break;
 
-        case "LEAVE" :
-        case "leave" :
+        case 3:
             window.alert("Leaving the store.");
 
             //do nothing so function will end
